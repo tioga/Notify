d7d631e6d6c7d579e2b93a519055cf604b58ea65
Jacob D. Parr <me@jacobparr.com>
2015-04-10 13:56:02 -0700

    Working distribution.

--- runtime/bin/run-server-jdp.bat
+++ runtime/bin/run-server-jdp.bat
@@ -1,4 +1,5 @@
 @echo off
-unzip .\build\distributions\lq-server-grizzly-trunk-SNAPSHOT.zip -d .\build\distributions\
-java -jar .\build\distributions\lq-server-grizzly-trunk-SNAPSHOT\lib\lq-server-grizzly.jar serverName localhost port 8080 shutdown 8081 context lq-server open true springFile config\spring-lq-server-core-jdp.xml
-echo LQ-Server has been shut down.
+cd ../../
+java -jar ./lib/tioga-notify-server-grizzly.jar
+echo Notify has been shut down.
+pause
