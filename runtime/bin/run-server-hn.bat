e3ef6603ac26cf6f9297e48095f0eabca450de60
Jacob D. Parr <me@jacobparr.com>
2015-04-22 14:36:31 -0700

    Removed batch files - replaced by dynamic server-start batch file.

--- runtime/bin/run-server-hn.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-unzip .\build\distributions\lq-server-grizzly-trunk-SNAPSHOT.zip -d .\build\distributions\
-java -jar .\build\distributions\lq-server-grizzly-trunk-SNAPSHOT\lib\lq-server-grizzly.jar notify.runtime_dir .. notify.config_name spring-config-hn.xml
-echo LQ-Server has been shut down.
--- runtime/bin/run-server-jdp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ../../
-java -jar ./lib/tioga-notify-server-grizzly.jar
-echo Notify has been shut down.
-pause
