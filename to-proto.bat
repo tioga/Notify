41d60de440f02bb9010e2cccbf4de95862c3a8df
Jacob D. Parr <me@jacobparr.com>
2015-05-04 12:36:52 -0700

    Updated project files, removed temporary batch file.

--- to-proto.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del \\parwinproto\d$\proto\tioga-notify-server-grizzly\lib\*.jar
-copy C:\dvlp\3rd-party\tioga-solutions\Notify\tioga-notify-server-grizzly\build\install\tioga-notify-server-grizzly\lib\*.jar \\parwinproto\d$\proto\tioga-notify-server-grizzly\lib\
-copy C:\dvlp\3rd-party\tioga-solutions\Notify\tioga-notify-server-grizzly\build\install\tioga-notify-server-grizzly\server-start.bat \\parwinproto\d$\proto\tioga-notify-server-grizzly\
