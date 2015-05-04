3a4a12c71b39224d13093921a526276fed7735e5
Jacob D. Parr <me@jacobparr.com>
2015-05-04 12:35:44 -0700

    Completed primary refactoring of initialization.

--- tioga-notify-server-grizzly/src/main/java/org/tiogasolutions/notify/server/grizzly/NotifyServer.java
+++ tioga-notify-server-grizzly/src/main/java/org/tiogasolutions/notify/server/grizzly/NotifyServer.java
@@ -13,24 +13,42 @@ import org.tiogasolutions.runners.grizzly.LoggerFacade;
 
 import java.nio.file.Path;
 
+import static org.slf4j.LoggerFactory.*;
+
 public class NotifyServer {
 
-  private static final Logger log = LoggerFactory.getLogger(NotifyServer.class);
+  private static final Logger log = getLogger(NotifyServer.class);
 
-  public static final String DEFAULT_SPRING_FILE = "/tioga-notify-server-grizzly/spring-config.xml";
+  public static final String DEFAULT_SPRING_FILE = "/org/tiogasolutions/notify/server/grizzly/spring-config.xml";
 
   public static void main(String...args) throws Exception {
 
+    // Priority #1, configure default logging levels. This will be overridden later
+    // when/if the logback.xml is found and loaded.
     LogUtils.initLogback(Level.WARN);
+    // Assume we want by default INFO on when & how the grizzly server is started
+    ((ch.qos.logback.classic.Logger) getLogger(NotifyServer.class)).setLevel(Level.INFO);
+    ((ch.qos.logback.classic.Logger) getLogger(GrizzlyServer.class)).setLevel(Level.INFO);
 
-    AppPathResolver resolver = new AppPathResolver(log::info, "notify.");
+    // Load the resolver which gives us common tools for identifying the
+    // runtime & config directories, logback.xml, etc.
+    AppPathResolver resolver = new AppPathResolver(getLogger(AppPathResolver.class)::info, "notify.");
+
     Path runtimeDir = resolver.resolveRuntimePath();
     Path configDir = resolver.resolveConfigDir(runtimeDir);
 
-    LogUtils.initLogback(configDir, "notify.log.config", "logback.xml");
+    // Re-init logback if we can find the logback.xml
+    Path logbackFile = LogUtils.initLogback(configDir, "notify.log.config", "logback.xml");
 
-    String springConfigPath = resolver.resolveSpringPath(configDir, "classpath:"+DEFAULT_SPRING_FILE);
-    String activeProfiles = resolver.resolveSpringProfiles();
+    // Locate the spring file for this app or use DEFAULT_SPRING_FILE from the classpath if one is not found.
+    String springConfigPath = resolver.resolveSpringPath(configDir, "classpath:" + DEFAULT_SPRING_FILE);
+    String activeProfiles = resolver.resolveSpringProfiles(); // defaults to "hosted"
+
+    log.info("Starting Notify Server:\n" +
+      "  *  Runtime Dir:  {}\n" +
+      "  *  Config Dir:   {}\n" +
+      "  *  Logback File: {}\n" +
+      "  *  Spring Path:  {}", runtimeDir, configDir, logbackFile, springConfigPath);
 
     // Create our application, initializing it with the specified spring file.
     NotifyApplication notifyApp = new NotifyApplication(activeProfiles, springConfigPath);
@@ -40,9 +58,9 @@ public class NotifyServer {
 
     // Create a facade around Slf4j for the server's initialization routines.
     LoggerFacade loggerFacade = new LoggerFacade() {
-      @Override public void info(String message) { log.info(message); }
-      @Override public void warn(String message) { log.warn(message); }
-      @Override public void error(String message, Throwable e) { log.error(message, e); }
+      @Override public void info(String message) { getLogger(GrizzlyServer.class).info(message); }
+      @Override public void warn(String message) { getLogger(GrizzlyServer.class).warn(message); }
+      @Override public void error(String message, Throwable e) { getLogger(GrizzlyServer.class).error(message, e); }
     };
 
     // Create an instance of the grizzly server.
