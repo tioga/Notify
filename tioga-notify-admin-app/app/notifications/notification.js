5ff46eeb57b6e524c15cc8dc0cfb40f276efc595
Harlan <harlan.work@gmail.com>
2016-02-01 22:54:43 -0800

    Show links and improve notification

--- tioga-notify-admin-app/app/notifications/notification.js
+++ tioga-notify-admin-app/app/notifications/notification.js
@@ -8,13 +8,20 @@ function Notification(domainName, data) {
     self.createdAt = app.mvc.observable(data.createdAtLocal);
     self.traitMap = app.mvc.observable(JSON.stringify(data.traitMap, null, ' '));
     self.exceptionInfo = new ExceptionInfo(data.exceptionInfo);
+    self.links = app.mvc.observableArray();
     self.attachments = app.mvc.observableArray();
     self.json = JSON.stringify(data, null, '\t');
+    $.each(data.links, function(i, link) {
+        self.links.push(new NotificationLink(link));
+    });
     $.each(data.attachmentInfoList, function(i, o) {
         self.attachments.push(new AttachmentInfo(domainName, data.notificationId, o));
     });
     self.hasException = app.mvc.computed(function() {
         return app.util.isNotEmpty(self.exceptionInfo.exceptionType());
+    });
+    self.hasLinks = app.mvc.computed(function() {
+        return self.links().length > 0;
     });
     self.hasAttachments = app.mvc.computed(function() {
         return self.attachments().length > 0;
@@ -27,8 +34,13 @@ function Notification(domainName, data) {
     self.showDetails = function() {
         app.dialogs.notificationDetail.show(self);
     };
+}
 
-
+function NotificationLink(data) {
+    var self = this;
+    data = (data != null) ? data : {};
+    self.name = app.mvc.observable(data.name);
+    self.href = app.mvc.observable(data.href);
 }
 
 function ExceptionInfo(data) {
