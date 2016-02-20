0780c9c7248fda38505769f41282baeaf36a7f95
Jacob D Parr <me@jacobparr.com>
2016-02-19 16:44:52 -0800

    Checkpoint

--- tioga-notify-extras-pwsmon/src/main/java/org/tiogasolutions/notify/extras/pwsmon/PwsMonitor.java
+++ tioga-notify-extras-pwsmon/src/main/java/org/tiogasolutions/notify/extras/pwsmon/PwsMonitor.java
@@ -2,16 +2,8 @@ package org.tiogasolutions.notify.extras.pwsmon;
 
 import ch.qos.logback.classic.Level;
 import org.slf4j.Logger;
-import org.tiogasolutions.app.common.AppPathResolver;
 import org.tiogasolutions.app.common.AppUtils;
-import org.tiogasolutions.runners.grizzly.GrizzlyServerConfig;
-import org.tiogasolutions.runners.grizzly.ShutdownUtils;
-import org.tiogasolutions.runners.grizzly.spring.ApplicationResolver;
-import org.tiogasolutions.runners.grizzly.spring.GrizzlySpringServer;
-import org.tiogasolutions.runners.grizzly.spring.ServerConfigResolver;
-
-import java.nio.file.Path;
-import java.util.Arrays;
+import org.tiogasolutions.dev.common.EnvUtils;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -19,7 +11,7 @@ public class PwsMonitor {
 
   private static final Logger log = getLogger(PwsMonitor.class);
 
-  public static void main(String...args) throws Exception {
+  public static void main(String...args) {
     // Priority #1, configure default logging levels. This will be
     // overridden later when/if the logback.xml is found and loaded.
     AppUtils.initLogback(Level.WARN);
@@ -27,49 +19,24 @@ public class PwsMonitor {
     // Assume we want by default INFO on when & how the grizzly server
     // is started. Possibly overwritten by logback.xml if used.
     AppUtils.setLogLevel(Level.INFO, PwsMonitor.class);
-    AppUtils.setLogLevel(Level.INFO, GrizzlySpringServer.GRIZZLY_CLASSES);
 
-    // Load the resolver which gives us common tools for identifying
-    // the runtime & config directories, logback.xml, etc.
-    AppPathResolver resolver = new AppPathResolver("pws.");
-    Path runtimeDir = resolver.resolveRuntimePath();
-    Path configDir = resolver.resolveConfigDir(runtimeDir);
+    try {
+      new PwsMonitor().run();
 
-    // Re-init logback if we can find the logback.xml
-    Path logbackFile = AppUtils.initLogback(configDir, "pws.log.config", "logback.xml");
-
-    // Locate the spring file for this app.
-    String springConfigPath = resolver.resolveSpringPath(configDir, "spring-pws-config.xml", "classpath:/tioga-notify-extras-pwsmon/spring-pws-config.xml");
-    String activeProfiles = resolver.resolveSpringProfiles(); // defaults to "hosted"
-
-    boolean shuttingDown = Arrays.asList(args).contains("-shutdown");
-    String action = (shuttingDown ? "Shutting down" : "Starting");
-
-    log.info("{} server:\n" +
-      "  *  Runtime Dir     (pws.runtime.dir)     {}\n" +
-      "  *  Config Dir      (pws.config.dir)      {}\n" +
-      "  *  Logback File    (pws.log.config)      {}\n" +
-      "  *  Spring Path     (pws.spring.config)   {}\n" +
-      "  *  Active Profiles (pws.active.profiles) {}", action, runtimeDir, configDir, logbackFile, springConfigPath, activeProfiles);
-
-    // Create an instance of the grizzly server.
-    GrizzlySpringServer grizzlyServer = new GrizzlySpringServer(
-      ServerConfigResolver.fromClass(GrizzlyServerConfig.class),
-      ApplicationResolver.fromClass(PwsApplication.class),
-      activeProfiles,
-      springConfigPath
-    );
-
-    grizzlyServer.packages("org.tiogasolutions.notify");
-
-    if (shuttingDown) {
-      ShutdownUtils.shutdownRemote(grizzlyServer.getConfig());
-      log.warn("Shut down server at {}:{}", grizzlyServer.getConfig().getHostName(), grizzlyServer.getConfig().getShutdownPort());
-      System.exit(0);
-      return;
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.flush();
     }
+    System.err.flush();
+    System.out.flush();
+    System.exit(0);
+  }
 
-    // Lastly, start the server.
-    grizzlyServer.start();
+  private void run() throws Exception {
+    String value = EnvUtils.requireProperty("delay");
+    long delay = Long.valueOf(value);
+
+
+    Thread.sleep(delay);
   }
 }
