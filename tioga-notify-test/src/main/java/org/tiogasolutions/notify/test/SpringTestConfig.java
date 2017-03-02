09af83ac33a019ebe3d2db9e682fb38c8e1dc318
Jacob D Parr <me@jacobparr.com>
2017-03-01 17:30:39 -0800

    Updated test-spring-config to use hosted CouchDB for unit tests.

--- tioga-notify-test/src/main/java/org/tiogasolutions/notify/test/SpringTestConfig.java
+++ tioga-notify-test/src/main/java/org/tiogasolutions/notify/test/SpringTestConfig.java
@@ -11,59 +11,75 @@ import org.tiogasolutions.notify.kernel.config.CouchServersConfig;
 import org.tiogasolutions.notify.kernel.config.SystemConfiguration;
 import org.tiogasolutions.notify.kernel.config.TrustedUserStore;
 
+import static org.tiogasolutions.dev.common.StringUtils.isNotBlank;
+
 @Profile("test")
 @Configuration
 public class SpringTestConfig {
 
-  @Bean
-  public NotifyObjectMapper notifyObjectMapper() {
-    return new NotifyObjectMapper();
-  }
+    @Bean
+    public NotifyObjectMapper notifyObjectMapper() {
+        return new NotifyObjectMapper();
+    }
 
-  @Bean
-  public TiogaJacksonTranslator tiogaJacksonTranslator(NotifyObjectMapper objectMapper) {
-    return new TiogaJacksonTranslator(objectMapper);
-  }
+    @Bean
+    public TiogaJacksonTranslator tiogaJacksonTranslator(NotifyObjectMapper objectMapper) {
+        return new TiogaJacksonTranslator(objectMapper);
+    }
 
-  @Bean
-  public CouchServersConfig couchServersConfig() {
+    @Bean
+    public CouchServersConfig couchServersConfig() {
 
-    CouchServersConfig config = new CouchServersConfig();
+        CouchServersConfig config = new CouchServersConfig();
 
-    config.setMasterUrl("http://127.0.0.1:5984");
-    config.setMasterUsername("test-user");
-    config.setMasterPassword("test-user");
-    config.setMasterDatabaseName("test-notify-master");
+        String couchUrl = "http://127.0.0.1:5984";
+        String username = "test-user";
+        String password = "test-user";
 
-    config.setNotificationUrl("http://127.0.0.1:5984");
-    config.setNotificationUsername("test-user");
-    config.setNotificationPassword("test-user");
-    config.setNotificationDatabasePrefix("test-notify-");
-    config.setNotificationDatabaseSuffix("-notification");
+        if (isNotBlank(System.getenv("awsCouchUrl"))) {
+            couchUrl = System.getenv("awsCouchUrl");
+        }
+        if (isNotBlank(System.getenv("awsCouchUsername"))) {
+            username = System.getenv("awsCouchUsername");
+        }
+        if (isNotBlank(System.getenv("awsCouchPassword"))) {
+            password = System.getenv("awsCouchPassword");
+        }
 
-    config.setRequestUrl("http://127.0.0.1:5984");
-    config.setRequestUsername("test-user");
-    config.setRequestPassword("test-user");
-    config.setRequestDatabasePrefix("test-notify-");
-    config.setRequestDatabaseSuffix("-request");
+        config.setMasterUrl(couchUrl);
+        config.setMasterUsername(username);
+        config.setMasterPassword(password);
+        config.setMasterDatabaseName("test-notify-master");
 
-    return config;
-  }
+        config.setNotificationUrl(couchUrl);
+        config.setNotificationUsername(username);
+        config.setNotificationPassword(password);
+        config.setNotificationDatabasePrefix("test-notify-");
+        config.setNotificationDatabaseSuffix("-notification");
 
-  @Bean
-  public TrustedUserStore trustedUserStore() {
-    return new TrustedUserStore(BeanUtils.toMap(
-      "admin:Testing123"
-    ));
-  }
+        config.setRequestUrl(couchUrl);
+        config.setRequestUsername(username);
+        config.setRequestPassword(password);
+        config.setRequestDatabasePrefix("test-notify-");
+        config.setRequestDatabaseSuffix("-request");
 
-  @Bean
-  public SystemConfiguration systemConfiguration() {
-    return new SystemConfiguration("*", "/api/v1/client", "/api/v1/admin");
-  }
+        return config;
+    }
 
-  @Bean
-  public CouchEnvironment couchEnvironment() {
-    return new CouchEnvironment().setTesting(true);
-  }
+    @Bean
+    public TrustedUserStore trustedUserStore() {
+        return new TrustedUserStore(BeanUtils.toMap(
+                "admin:Testing123"
+        ));
+    }
+
+    @Bean
+    public SystemConfiguration systemConfiguration() {
+        return new SystemConfiguration("*", "/api/v1/client", "/api/v1/admin");
+    }
+
+    @Bean
+    public CouchEnvironment couchEnvironment() {
+        return new CouchEnvironment().setTesting(true);
+    }
 }
